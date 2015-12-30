5c418b3ba77577cc6e2a4a37acba3f5fc4908670
Obie Fernandez <obie@andela.co>
2015-12-29 23:51:28 -0500

    added test coverage. big thanks to Andy Maleh

--- tests/sns.js
+++ tests/sns.js
@@ -1,13 +1,26 @@
-var AWS = require('aws-sdk');
-var Chainsaws = new require('../lib/chainsaws.js')(AWS);
+var chai = require('chai');
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+chai.use(sinonChai);
+var expect = chai.expect;
 
-Chainsaws.config.update({region:'us-east-1'});
+var AWS = require('aws-sdk');
+AWS.config.update({region:'us-east-1'});
+
+var Chainsaws = new require('../lib/chainsaws.js')(AWS);
 
 describe('SNS functions', function() {
   describe('publish', function() {
     it('should put a message on a topic', function(done) {
-      var topic = new Chainsaws.Topic("someTopicArn");
+      var publishStub = sinon.stub(Chainsaws.sns, 'publish', function(obj, cb) {cb()});
+      var topicArn = "someTopicArn";
+      var topic = new Chainsaws.Topic(topicArn);
       topic.publish({foo: "bar"}, done);
+
+      expect(publishStub).to.have.been.calledWith({
+            TopicArn: topicArn,
+            Message: "{\"foo\":\"bar\"}"
+          }, done);
     });
   });
 });
