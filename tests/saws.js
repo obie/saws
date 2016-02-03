32a5a3b8c499441927f0ae2b9702d8b1c6576648
Timothy Caraballo <openback@gmail.com>
2016-02-03 16:38:47 -0500

    Add DEBUG test and code coverage with istanbul

--- tests/saws.js
+++ tests/saws.js
@@ -10,10 +10,14 @@ var expect = chai.expect;
 var AWSStub = require('./aws-stub');
 
 describe('Saws', function() {
-  it('should autoload submodules', function(done) {
-    var Saws = require('../lib/saws').Saws;
-    var saws = new Saws(require('./aws-stub')());
+  var saws;
 
+  beforeEach(function() {
+    var Saws = require('../lib/saws').Saws;
+    saws = new Saws(require('./aws-stub')());
+  });
+
+  it('should autoload submodules', function(done) {
     var expectedModules = [
       'Queue',
       'Topic',
@@ -24,6 +28,30 @@ describe('Saws', function() {
     expectedModules.forEach(function(moduleName) {
         expect(saws[moduleName]).to.be.instanceof(Function, util.format('saws.%s not found', moduleName));
     });
+    done();
+  });
+
+  it('should only log in debug mode', function(done) {
+    var original = process.env.SAWS_DEBUG;
+    delete process.env.SAWS_DEBUG;
+
+    sinon.spy(console, 'log');
+    saws.DEBUG('');
+
+    expect(console.log).to.not.have.been.called;
+
+    process.env.SAWS_DEBUG = true;
+    saws.DEBUG('');
+    expect(console.log).to.have.been.called;
+
+    console.log.restore();
+
+    if (original) {
+      process.env.SAWS_DEBUG = original;
+    } else {
+      delete process.env.SAWS_DEBUG;
+    }
+
     done();
   });
 });
